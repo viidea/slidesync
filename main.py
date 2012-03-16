5dc56815159320c6e6ab6841750166e7ff64a5e3
Jernej Virag <jernej.virag@gmail.com>
2012-03-16 15:30:19 +0100

    Video info extraction

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import logging
 import sys
 from PyQt4 import QtGui
-from PyQt4.QtCore import QDir
-from video import VideoFile
+from PyQt4.QtCore import QDir, Qt, QString
+from PyQt4.QtGui import QMessageBox
+import video
+
+logger = logging.getLogger(__name__)
 
 class MainWindow(QtGui.QMainWindow):
     video_file = None
@@ -10,33 +14,55 @@ class MainWindow(QtGui.QMainWindow):
         super(MainWindow, self).__init__()
         self.setGeometry(300, 300, 640, 480)
         self.setWindowTitle("VSync")
-        self.show()
 
+        # Prepare toolbar
         loadFileAction = QtGui.QAction("Load video...", self)
         loadFileAction.setShortcut("Ctrl+L")
         loadFileAction.triggered.connect(self.click_load_video)
-
         self.toolbar = self.addToolBar("Main")
         self.toolbar.addAction(loadFileAction)
+
+        # Prepare status bar
+        self.status_label = QtGui.QLabel("Status")
+        self.status_label.setAlignment(Qt.AlignLeft)
+        self.statusBar().addWidget(self.status_label, stretch=1)
+
+        self.video_label = QtGui.QLabel("")
+        self.video_label.setMinimumWidth(150)
+        self.video_label.setAlignment(Qt.AlignRight)
+        self.statusBar().addWidget(self.video_label)
         self.status_ready()
 
+        self.show()
 
     def click_load_video(self):
         filename = QtGui.QFileDialog().getOpenFileName(self, "Open video file", QDir.homePath())
         if filename == "":
             return      # User canceled the dialog
 
-        self.statusBar().showMessage("Loading %s..." % (filename,))
-        self.video_file = VideoFile(filename)
+        self.status_label.setText("Loading %s..." % (filename,))
+
+        try:
+            self.video_file = video.VideoFile(filename)
+        except video.VideoException as e:
+            msgBox = QMessageBox(QMessageBox.Critical, "Error :(", "Video file could not be opened.")
+            msgBox.setDetailedText(unicode(e))
+            msgBox.exec_()
+            return
+
         self.status_ready()
+        self.video_label.setText(unicode(self.video_file.get_info()))
 
     def status_ready(self):
-        self.statusBar().showMessage("Ready.")
+        self.status_label.setText("Ready.")
+        self.status_label.update()
 
 def main():
     """
     Main application entry point
     """
+    logging.basicConfig()
+
     app = QtGui.QApplication(sys.argv)
     window = MainWindow()
     sys.exit(app.exec_())
--- video.py
+++ video.py
@@ -1,4 +1,55 @@
+import logging
+import pyglet
+
+logger = logging.getLogger(__name__)
+
+class VideoException(Exception):
+    pass
+
+class VideoLoadException(VideoException, IOError):
+    pass
+
+class VideoInfo(object):
+    width = None
+    height = None
+    audio_channels = None
+    audio_samplerate = None
+
+    def __init__(self, width, height, channels, samplerate):
+        self.width = width
+        self.height = height
+        self.audio_channels = channels
+        self.audio_samplerate = samplerate
+
+    def __unicode__(self):
+        return self.__str__()
+
+    def __str__(self):
+        return "[V:%sx%s][A:%sch %sHz]" % (self.width, self.height, self.audio_channels, self.audio_samplerate)
 
 class VideoFile(object):
+    info = None
+    current_frame = None
+    current_frame_timestamp = None
+
     def __init__(self, filepath):
-        self.filepath = filepath
+        self.filepath = unicode(filepath)
+        self.load()
+
+    def load(self):
+        try:
+            self.source = pyglet.media.load(self.filepath)
+        except pyglet.media.MediaException as e:
+            raise VideoLoadException("Unknown video format.\n%s" % (e,))
+        except IOError as e:
+            raise VideoLoadException("Could not load video file.\n%s" % (e,))
+
+        self.info = VideoInfo(self.source.video_format.width, self.source.video_format.width, self.source.audio_format.channels, self.source.audio_format.sample_rate)
+
+    def get_frame(self):
+        self.current_frame_timestamp = self.source.get_next_video_timestamp()
+        self.current_frame = self.source.get_next_video_frame()
+        logger.debug("[VideoFile] Got frame at %s." % (self.current_frame_timestamp,))
+
+    def get_info(self):
+        return self.info
