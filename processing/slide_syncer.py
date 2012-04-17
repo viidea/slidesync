1db9bbba9be480a355c8df656a92396963c87d7c
Jernej Virag <jernej.virag@gmail.com>
2012-04-17 12:31:04 +0200

    Assert and timing fixes

--- processing/slide_syncer.py
+++ processing/slide_syncer.py
@@ -14,13 +14,13 @@ class SlideSyncer(object):
         # Load audio files first
         original_audio, original_sr = utils.get_audio_from_file(self.original_video_file)
         slide_audio, slide_sr = utils.get_audio_from_file(self.slide_video_file)
+        assert original_sr == slide_sr
 
         logger.debug("Preprocessing info...")
         # Preprocess audio files
         original_audio, original_sr = sync.preprocess_audio(original_audio, original_sr)
         slide_audio, slide_sr = sync.preprocess_audio(slide_audio, slide_sr)
 
-        assert original_sr == slide_sr
         if self.global_method:
             updated_slides = self._get_synced_timings_global(slide_data, original_audio, slide_audio, slide_sr)
         else:
@@ -29,12 +29,12 @@ class SlideSyncer(object):
         return updated_slides
 
     def _get_synced_timings_global(self, slide_data, original_audio, slide_audio, samplerate):
-        global_offset, corr = correlate.get_offset(original_audio, slide_audio, samplerate)
+        global_offset, corr = correlate.get_offset(slide_audio, original_audio, samplerate)
         print "Found offset %s" % global_offset
 
         updated_slides = []
         for slide_time, slide_name in slide_data.items():
-            slide_time = max(0, slide_time - global_offset)
+            slide_time = max(0, slide_time + global_offset)
             updated_slides.append((slide_time, slide_name))
         return updated_slides
 
